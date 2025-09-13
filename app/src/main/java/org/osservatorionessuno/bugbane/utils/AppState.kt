8fd07caedad1a6af7f18c846ba1b08c141a1c620
Rowen S <ro@freedom.press>
2025-09-13 10:08:51 -0400

    Handle connected, error, and pairing states in scan screen via adbState. Add TryAutoConnect state to AppState and include adbManager logic to support it. Add additional transitional adb and app states. Improve wifi connectivity monitor checks.
    Use checkState in AdbManager to report connection.

--- app/src/main/java/org/osservatorionessuno/bugbane/utils/AppState.kt
+++ app/src/main/java/org/osservatorionessuno/bugbane/utils/AppState.kt
@@ -1,34 +1,41 @@
 package org.osservatorionessuno.bugbane.utils
 
 // AppStates represent the high-level device configuration and permissions status;
-// state ordering/requisites are defined in the ViewModel (see ConfigurationViewModel).
+// state requisites are defined in the ViewModel (see ConfigurationViewModel).
 // An AppState requires user interaction to change to a different state.
-enum class AppState(val index: Int) {
-    DeviceUnsupported(8),
-    NeedWelcomeScreen(0), // one-time consent
-    NeedNotificationConfiguration(1),
+private const val EXCLUDED_STEP = 999
+enum class AppState(val step: Int) {
+    NeedWelcomeScreen(0),
+    NeedNotificationPermission(1),
+    DeviceUnsupported(1), // Alternative to step 1: device isn't compatible
     NeedWifi(2),
     NeedDeveloperOptions(3),
-    NeedWirelessDebuggingAndPair(4), // sub-states are handled by AdbManager
-//    NeedAdbPairingService(5),
-    AdbConnectedFinishOnboarding(6), // one-time onboarding complete
-    AdbConnected(7);
+
+    // Step 4: ADB/Wireless ADB/Pairing
+    NeedWirelessDebuggingAndPair(4),
+    NeedWirelessDebugging(4),
+
+    // Step 5: ADB connection attempt
+    AdbConnecting(5),
+    AdbConnectedFinishOnboarding(5),
+    AdbConnected(5),
+    TryAutoConnect(5),
+    AdbConnectionError(5),
+
+    // Not part of our slideshow
+    AdbScanning(EXCLUDED_STEP);
 
     companion object {
-        fun valuesInOrder(): List<AppState> = listOf(
-            NeedWelcomeScreen,
-            NeedNotificationConfiguration,
-            NeedWifi,
-            NeedDeveloperOptions,
-            NeedWirelessDebuggingAndPair,
-//            NeedAdbPairingService,
-            AdbConnectedFinishOnboarding,
-            AdbConnected,
-            DeviceUnsupported,
-            )
         // Error states have different UI implications
         fun isErrorState(state: AppState): Boolean {
-            return (state == AppState.DeviceUnsupported)
+            return (state in arrayOf(DeviceUnsupported, AdbConnectionError))
+        }
+        fun distinctSteps(): Int {
+            return entries
+                .map { it.step }
+                .filterNot { it == EXCLUDED_STEP }
+                .toSet()
+                .size
         }
     }
 }
